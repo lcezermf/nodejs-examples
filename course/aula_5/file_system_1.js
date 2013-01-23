f8cbe374398a04c0efd1a5a9cb4df9410492f223
lccezinha <lccezinha@gmail.com>
2013-01-23 01:04:51 -0200

    file system - class 05 almost done, ALMOST.

---
+++ course/aula_5/file_system_1.js
@@ -0,0 +1,30 @@
+var fs = require('fs')
+
+/* Lendo do arquivo 
+
+fs.readFile('./arquivo.txt', function(error, data) {
+  if (error) throw error;
+  console.log(data.toString('utf8'));
+});
+
+*/
+
+/* Escrever no arquivo
+
+fs.writeFile('message.txt', "Hello NodeJS\n\n", function(error) {
+  if(error) throw error;
+  console.log('Salvo com sucesso!');
+});
+
+*/
+
+
+/* Append no arquivo 
+
+fs.appendFile('message.txt', "Segunda linha do arquivo\n\n", 'utf-8', function(error) {
+  if (error) throw error;
+  console.log('Append realizado');
+});
+
+*/
+
---
+++ course/aula_5/file_system_2.js
@@ -0,0 +1,21 @@
+var fs = require('fs')
+
+//status
+/*
+function mostrar_status(dir, file_name){
+	fs.lstat(dir + '/' + file_name, function(error, stats) {
+    //console.log(stats);
+    console.log('É um diretório : ' + stats.isDirectory());
+    console.log('É uma arquivo : ' + stats.isFile());
+    console.log('É um link simbólico : ' + stats.isSymbolicLink());
+	});
+}
+
+mostrar_status(__dirname, 'other');
+*/
+
+//exibe informações do arquivo em 'real-time'
+fs.watchFile('./message.txt', function(current, previous) {
+  console.log(current);
+  console.log(previous);
+});
