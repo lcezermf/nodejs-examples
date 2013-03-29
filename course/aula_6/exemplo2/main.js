1e38cbe53a47aae5629d91a514b1a027bc2a9d42
lccezinha <lccezinha@gmail.com>
2013-03-29 02:15:37 -0300

    class 11, class 12 DONE

---
+++ course/aula_6/exemplo2/main.js
@@ -0,0 +1,17 @@
+/* modulo1.js */
+var modulo = require('./modulo1.js');
+modulo.diga_nome('Cezinha');
+modulo.diga_idade(22);
+
+/* modulo2.js 
+
+//da maneira como está o modulo2, será criado um objeto Pessoa.
+
+var Pessoa = require('./modulo2.js');
+var pessoa = new Pessoa('Luiz', 22);
+
+pessoa.imprime_nome();
+pessoa.imprime_idade();
+pessoa.maior_idade();
+
+*/
