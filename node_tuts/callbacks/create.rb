faba6e34fbb9d4fa7ed9e44653631a859f89f0ab
Luiz Cezer <lccezinha@gmail.com>
2013-08-15 21:20:19 -0300

    removing olds files

--- node_tuts/callbacks/create.rb
+++
@@ -1,3 +0,0 @@
-1000.times do |ite|
-  File.open("file.txt", "a+") { |file| file.puts ite.to_s }
-end 
--- node_tuts/callbacks/live.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-
-fs.readFile("./file.txt", function(err, data) {
-  if (err) console.log(err);
-  console.log('data %s', data);
-});
